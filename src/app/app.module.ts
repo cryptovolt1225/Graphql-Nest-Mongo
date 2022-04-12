ee21525dc3a2c5968efa02a288cf96d6db033ac4
Crypto_Volt <ivan.kuzimin15@gmail.com>
2022-04-12 10:56:45 +0900

    construct person&hobby content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PersonModule } from './person/person.module';
+import { HobbyModule } from './hobby/hobby.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/three-in-one-db'),
+    PersonModule,
+    HobbyModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/app/person/person.model.ts
@@ -0,0 +1,7 @@
+import { Hobby } from "./hobby.model";
+
+export class Person {
+  _id: String;
+  name: String;
+  hobbies: Hobby[];
+}
