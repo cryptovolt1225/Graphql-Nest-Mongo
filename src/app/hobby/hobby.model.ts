e7df140d52095fbb295ab42db5a839f3be9f29b7
Crypto_Volt <ivan.kuzimin15@gmail.com>
2022-04-12 11:49:19 +0900

    added graphql module & changed models

--- src/app/hobby/hobby.model.ts
+++ src/app/hobby/hobby.model.ts
@@ -1,10 +1,15 @@
 import {Document, Schema as MongooseSchema} from 'mongoose';
 import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+import { Field, ObjectType } from '@nestjs/graphql'; 
 
+@ObjectType()
 @Schema()
 export class Hobby {
+
+  @Field(() => String)
   _id: MongooseSchema.Types.ObjectId;
 
+  @Field(() => String)
   @Prop()
   name: String;
 }
--- src/app/person/person.model.ts
+++ src/app/person/person.model.ts
@@ -1,15 +1,20 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Schema as MongooseSchema} from 'mongoose';
+import { Field, ObjectType} from '@nestjs/graphql';
 
 import { Hobby } from "../hobby/hobby.model";
 
+@ObjectType()
 @Schema()
 export class Person {
+  @Field(() => String)
   _id: MongooseSchema.Types.ObjectId;
 
+  @Field(() => String)
   @Prop()
   name: String;
 
+  @Field(() => [String])
   @Prop({type: [MongooseSchema.Types.ObjectId], ref: Hobby.name})
   hobbies: MongooseSchema.Types.ObjectId[];
 }
